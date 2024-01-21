8fdebb4812ae2e9709b0f1f774d6d9d5c0766995
harshitshh <harshitsh7@yahoo.com>
2024-01-21 16:24:59 +0530

    commit

---
+++ accending.cpp
@@ -0,0 +1,11 @@
+//printing numbers from 1 to input number n
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1,n;
+    cout<< "Enter the Number\n";
+    cin>>n;
+    while(i<=n){
+        cout<<endl<<i;
+        i++;
+    }}
---
+++ decending.cpp
@@ -0,0 +1,11 @@
+//printing Whole numbers in decending order from N.
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter the Number\n";
+    cin>>n;
+    while(n>0){
+        cout<<endl<<n;
+        n--;
+    }}
